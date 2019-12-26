030aaacaa0f535b8e20beff54a445a3c9b797953
Hieu Nguyen <hieunv@live.com>
2019-12-26 16:22:21 +0700

    v0.0.6-dev

---
+++ precache-manifest.3bc9f68838fee6925fa9a8d1c94a2643.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860ba0628148b0fc8232",
+    "url": "build/2.860ba062.js"
+  },
+  {
+    "revision": "9d1d953fdaf16ebb2bed4a2cb8f0f301",
+    "url": "build/2.860ba062.js.LICENSE"
+  },
+  {
+    "revision": "50416cb95f741838b7f7",
+    "url": "build/bundle.50416cb9.js"
+  },
+  {
+    "revision": "a0d77c29d60918b8fc08",
+    "url": "build/main.a0d77c29.js"
+  },
+  {
+    "revision": "e10efa3a04f908d6d8fb8e6cf749b5d3",
+    "url": "index.html"
+  }
+]);
